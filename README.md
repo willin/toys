83b1eb7f23c7f0e0944bc7ff22cfab8eed30f2e6
Willin Wang <willin@willin.org>
2017-08-27 18:28:41 +0800

    add toys

--- README.md
+++ README.md
@@ -18,15 +18,44 @@ A simple vue project to manage toys I wanna buy and already bought. https://toys
 
 ## Keyboards & Mouse 
 
+- [ ] GH40 (40% Cyan Switch)
+- [ ] Filco 87 (80% Cyan Switch)
+- [x] Qmkeyboard (60% Cyan Switch) (2017)
+- [x] Logitech Master 2S (2017)
+- [x] <del>Logitech MX Master</del> (2016)
+- [x] HHKB Pro 2 Type-S (60% Capacitive) (2016)
+- [x] <del>Magic Mouse 2</del> (2016)
+- [x] ikbc Poker 3 (100% Green Switch) (2015)
+- [x] Cherry G80-1869HYNUS-2 (100% Red Switch) (2014)
+- [x] <del>Mad Catz R.A.T.9</del> (2013)
+- [x] <del>Magic Mouse</del> (2011)
 
 ## Phones 
 
+- [ ] iPhone 8
+- [x] iPhone 7 plus (2017)
+- [x] <del>Samsung Galaxy S7</del> (2016)
+- [x] <del>iPhone 6s</del> (2016)
+- [x] <del>Samsung Galaxy S6</del> (2016)
+- [x] <del>iPhone 6 plus</del> (2014)
+- [x] <del>iPhone 5</del> (2012)
+- [x] <del>iPhone 4s</del> (2011)
+- [x] <del>Nokia N97</del> (2010)
+- [x] <del>Samsung Anycall i458</del> (2008)
 
 ## Wrist Devices 
 
+- [x] Apple Watch S2 (42mm) (2017)
+- [x] <del>Garmin Forerunner 225</del> (2015)
+- [x] <del>Nike+ FuelBand 2</del> (2014)
+- [x] <del>Fitbit Flex</del> (2013)
 
 ## Geeks 
 
+- [x] Phlips 288P6LJEB (4K, 28') (2016)
+- [x] AOC C3583FQ/WS (2K, 35') (2016)
+- [x] <del>Kindle Voyage</del> (2015)
+- [x] <del>AOC U2870VQE (4k, 28')</del> (2014)
 
 
 ## License
--- toys.js
+++ toys.js
@@ -57,17 +57,188 @@
     {
       category: 'Keyboards & Mouse',
       items: [
-
+        {
+          name: 'GH40',
+          meta: '40% Cyan Switch'
+        },
+        {
+          name: 'Filco 87',
+          meta: '80% Cyan Switch'
+        },
+        {
+          name: 'Qmkeyboard',
+          meta: '60% Cyan Switch',
+          year: 2017,
+          bought: true
+        },
+        {
+          name: 'Logitech Master 2S',
+          year: 2017,
+          bought: true
+        },
+        {
+          name: 'Logitech MX Master',
+          year: 2016,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'HHKB Pro 2 Type-S',
+          meta: '60% Capacitive',
+          year: '2016',
+          bought: true
+        },
+        {
+          name: 'Magic Mouse 2',
+          year: '2016',
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'ikbc Poker 3',
+          meta: '100% Green Switch',
+          year: '2015',
+          bought: true
+        },
+        {
+          name: 'Cherry G80-1869HYNUS-2',
+          meta: '100% Red Switch',
+          year: '2014',
+          bought: true
+        },
+        {
+          name: 'Mad Catz R.A.T.9',
+          year: '2013',
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Magic Mouse',
+          year: '2011',
+          bought: true,
+          deleted: true
+        }
       ]
     },
     {
-      category: 'Phones'
+      category: 'Phones',
+      items: [
+        {
+          name: 'iPhone 8'
+        },
+        {
+          name: 'iPhone 7 plus',
+          year: 2017,
+          bought: true
+        },
+        {
+          name: 'Samsung Galaxy S7',
+          year: 2016,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'iPhone 6s',
+          year: 2016,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Samsung Galaxy S6',
+          year: 2016,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'iPhone 6 plus',
+          year: 2014,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'iPhone 5',
+          year: 2012,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'iPhone 4s',
+          year: 2011,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Nokia N97',
+          year: 2010,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Samsung Anycall i458',
+          year: 2008,
+          bought: true,
+          deleted: true
+        }
+      ]
     },
     {
-      category: 'Wrist Devices'
+      category: 'Wrist Devices',
+      items: [
+        {
+          name: 'Apple Watch S2',
+          meta: '42mm',
+          year: 2017,
+          bought: true
+        },
+        {
+          name: 'Garmin Forerunner 225',
+          year: 2015,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Nike+ FuelBand 2',
+          year: 2014,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'Fitbit Flex',
+          year: 2013,
+          bought: true,
+          deleted: true
+        }
+      ]
     },
     {
-      category: 'Geeks'
+      category: 'Geeks',
+      items: [
+        {
+          name: 'Phlips 288P6LJEB',
+          meta: '4K, 28\'',
+          year: 2016,
+          bought: true
+        },
+        {
+          name: 'AOC C3583FQ/WS',
+          meta: '2K, 35\'',
+          year: 2016,
+          bought: true
+        },
+        {
+          name: 'Kindle Voyage',
+          year: 2015,
+          bought: true,
+          deleted: true
+        },
+        {
+          name: 'AOC U2870VQE',
+          meta: '4k, 28\'',
+          year: 2014,
+          bought: true,
+          deleted: true
+        }
+      ]
     }
   ];
 }(typeof window === 'undefined' ? exports : window));
